2332b1696d4071c3e98a7240f20741e42b95656d
Khushal Abrol <khushalabrol14@gmail.com>
2020-06-19 16:58:58 +0530

    initial commit

---
+++ src/server/index.js
@@ -0,0 +1,45 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+
+
+
+
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
+
+
+
+
+var aylien = require("aylien_textapi");
+
+var textapi = new aylien({
+    application_id: process.env.APP_ID,
+    application_key: process.env.APP_KEY
+});
+
+console.log(`Your API key is ${process.env.API_KEY}`);
